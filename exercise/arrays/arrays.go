b44612b8c9f029b9f8072f5ffa3436a05210f527
Nde Lucien <chelucien08@gmail.com>
2024-07-04 21:36:43 +0100

    lesson: arrays

--- exercise/arrays/arrays.go
+++ exercise/arrays/arrays.go
@@ -18,6 +18,41 @@ package main
 
 import "fmt"
 
+type Product struct {
+	name  string
+	price int
+}
+
+func displayDetails(shoppingList [4]Product) {
+	fmt.Println("===> Full Shopping List ===>:", shoppingList)
+
+	var sum, totalItems int
+
+	for i := 0; i < len(shoppingList); i++ {
+		item := shoppingList[i]
+		sum += item.price
+		if item.name != "" {
+			totalItems += 1
+		}
+	}
+
+	fmt.Println("Sum total price of items:", sum)
+	fmt.Println("The total number of items:", totalItems)
+	fmt.Println("The last item is:", shoppingList[totalItems-1])
+
+}
+
 func main() {
+
+	var shoppingList [4]Product
+
+	shoppingList[0] = Product{"Shoes", 5}
+	shoppingList[1] = Product{"TV", 45}
+	shoppingList[2] = Product{"Umbrella", 2}
+
+	displayDetails(shoppingList)
+
+	shoppingList[3] = Product{"Phone", 67}
+	displayDetails(shoppingList)
 
 }
