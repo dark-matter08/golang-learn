921db8c9c580e1d47ef97932f613ba3bdf0dbf25
Nde Lucien <chelucien08@gmail.com>
2024-07-05 13:13:11 +0100

    lesson: maps

--- demo/maps/maps.go
+++ demo/maps/maps.go
@@ -3,4 +3,35 @@ package main
 import "fmt"
 
 func main() {
+
+	shoppingList := make(map[string]int)
+	shoppingList["eggs"] = 11
+	shoppingList["milk"] = 3
+	shoppingList["bread"] += 1
+
+	shoppingList["eggs"] += 1
+
+	fmt.Println(shoppingList)
+
+	delete(shoppingList, "milk")
+	fmt.Println("Milk deleted, new list: ", shoppingList)
+
+	fmt.Println("need", shoppingList["eggs"], "eggs")
+
+	cereal, found := shoppingList["cereal"]
+
+	if !found {
+		println("Nope")
+	} else {
+		println("Yup", cereal, "boxes")
+	}
+
+	var totalItems int
+
+	for _, amount := range shoppingList {
+		totalItems += amount
+	}
+
+	fmt.Println("Total Items:", totalItems)
+
 }
--- exercise/maps/maps.go
+++ exercise/maps/maps.go
@@ -26,6 +26,69 @@ const (
 	Retired     = 3
 )
 
+func serverStatus(servers map[string]int) {
+	fmt.Println()
+	fmt.Println("Server Information")
+	fmt.Println("Number of servers:", len(servers))
+
+	stats := make(map[int]int)
+
+	for server, status := range servers {
+		switch status {
+		case Online:
+			stats[Online] += 1
+		case Offline:
+			stats[Offline] += 1
+		case Maintenance:
+			stats[Maintenance] += 1
+		case Retired:
+			stats[Retired] += 1
+		default:
+			panic("Unhandled server status")
+		}
+
+		var statusName string
+		if status == 0 {
+			statusName = "Online"
+		}
+		if status == 1 {
+			statusName = "Offline"
+		}
+		if status == 2 {
+			statusName = "Maintenance"
+		}
+		if status == 3 {
+			statusName = "Retired"
+		}
+		fmt.Println("==> Server Status:", server, "is ->", statusName)
+	}
+
+	fmt.Println("=>", stats[Online], "Servers are Online")
+	fmt.Println("=>", stats[Offline], "Servers are Offline")
+	fmt.Println("=>", stats[Maintenance], "Servers are under Maintenance")
+	fmt.Println("=>", stats[Retired], "Servers are Retired")
+
+}
+
 func main() {
 	servers := []string{"darkstar", "aiur", "omicron", "w359", "baseline"}
+
+	serverMap := make(map[string]int)
+
+	for _, server := range servers {
+		serverMap[server] = Online
+	}
+
+	serverStatus(serverMap)
+
+	serverMap["darkstar"] = Retired
+	serverMap["aiur"] = Offline
+	serverStatus(serverMap)
+
+	for server := range serverMap {
+		serverMap[server] = Maintenance
+	}
+
+	serverStatus(serverMap)
+
 }
