312462c24cd6938fed1a5664c445be0c9152a209
Nde Lucien <chelucien08@gmail.com>
2024-07-15 13:02:09 +0100

    lesson: receiver functions

--- exercise/rcv-func/rcv-func.go
+++ exercise/rcv-func/rcv-func.go
@@ -16,6 +16,93 @@ package main
 
 import "fmt"
 
+type Health struct {
+	value uint
+	max   uint
+}
+
+type Energy struct {
+	value uint
+	max   uint
+}
+
+type Player struct {
+	health Health
+	energy Energy
+	name   string
+}
+
+func (player *Player) addHealth(increment uint) {
+	fmt.Println("Increasing player health by", increment)
+
+	if player.health.value+increment >= player.health.max {
+		player.health.value = player.health.max
+		fmt.Println("Player stats after change:", player)
+
+		return
+	}
+
+	player.health.value += increment
+	fmt.Println("Player stats after change:", player)
+
+}
+
+func (player *Player) addEnergy(increment uint) {
+	fmt.Println("Increasing player Energy by", increment)
+
+	if player.energy.value+increment >= player.energy.max {
+		player.energy.value = player.energy.max
+		fmt.Println("Player stats after change:", player)
+
+		return
+	}
+
+	player.energy.value += increment
+	fmt.Println("Player stats after change:", player)
+
+}
+
+func (player *Player) reduceHealth(decrement uint) {
+	fmt.Println("Decreasing player health by", decrement)
+
+	if player.health.value-decrement > player.health.value {
+		player.health.value = 0
+		fmt.Println("Player stats after change:", player)
+
+		return
+	}
+
+	player.health.value -= decrement
+	fmt.Println("Player stats after change:", player)
+
+}
+
+func (player *Player) reduceEnergy(decrement uint) {
+	fmt.Println("Decreasing player energy by", decrement)
+
+	if player.energy.value-decrement > player.energy.value {
+		player.energy.value = 0
+		fmt.Println("Player stats after change:", player)
+
+		return
+	}
+
+	player.energy.value -= decrement
+	fmt.Println("Player stats after change:", player)
+
+}
+
 func main() {
+
+	player := Player{name: "Nde Lucien", health: Health{value: 10, max: 15}, energy: Energy{value: 12, max: 15}}
+
+	fmt.Println("Initial Player:", player)
+
+	player.addHealth(1)
+	player.addHealth(1)
+	player.reduceHealth(5)
+	player.addEnergy(3)
+	player.reduceEnergy(8)
+	player.reduceEnergy(20)
 
 }
